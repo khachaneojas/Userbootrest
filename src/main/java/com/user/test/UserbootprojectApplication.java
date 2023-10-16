91fc57202abb0735e293839a31f06d1f6d7b4fab
khachaneojas <khachaneojas@gmail.com>
2023-10-16 19:23:12 +0530

    Created Api-Gateway & Logging with SL4J
    Created api-Gateway project and registered it with eureka server & configured logging in application.properties

--- src/main/java/com/user/test/UserbootprojectApplication.java
+++ src/main/java/com/user/test/UserbootprojectApplication.java
@@ -2,9 +2,11 @@ package com.user.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = "com.user.test")
+@EnableEurekaClient
 public class UserbootprojectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/user/test/controller/UserController.java
+++ src/main/java/com/user/test/controller/UserController.java
@@ -2,18 +2,13 @@ package com.user.test.controller;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.user.test.annotation.Auditor;
 import com.user.test.enums.Authority;
@@ -29,6 +24,7 @@ import com.user.test.service.UserService;
 import com.user.test.util.JwtUtil;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 	
 	@Autowired
@@ -39,11 +35,14 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository; 
-	
-	@GetMapping("/users")
+
+	Logger log = LoggerFactory.getLogger(UserController.class);
+
+	@GetMapping("/allusers")
 //	@Secured({"Role_Admin"})
 //	@PreAuthorize("hasAuthority('Role_Admin')")
 	public ResponseEntity<?> getallUser(@RequestHeader(HttpHeaders.AUTHORIZATION) String str){
+		log.info("ABC");
 		return new ResponseEntity<>(userService.getAllUsers(str), HttpStatus.OK);
 	}
 
